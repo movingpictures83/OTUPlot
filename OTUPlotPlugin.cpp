1e23a5e2929f5001a13f7a728959983aec14229a
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-05-07 12:56:55 -0400

    Removed environment variables Added 1% error check

--- OTUPlotPlugin.cpp
+++ OTUPlotPlugin.cpp
@@ -21,8 +21,8 @@ void OTUPlotPlugin::run() {
 
 void OTUPlotPlugin::output(std::string file) {
 //beta_diversity.py -i filtered_otu_table.biom -m euclidean,weighted_unifrac,unweighted_unifrac -t rep_set.tre -o beta_div
-   std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; rm -rf "+file+"; summarize_taxa_through_plots.py ";
- command += "-i "+parameters["biomfile"]+" -m "+parameters["mapping"]+" -o "+file+"; cp "+file+"/* "+file+"/..";
+   std::string command = /*"export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; rm -rf "+file+";*/ "summarize_taxa_through_plots.py ";
+ command += "-i "+parameters["biomfile"]+" -m "+parameters["mapping"]+" -o "+file+" -f; cp "+file+"/* "+file+"/..";
  std::cout << command << std::endl;
  system(command.c_str());
 }
